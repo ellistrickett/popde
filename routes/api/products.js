41c72559a000ac5d40ff06afe922fbcfc014ae76
Ellis Trickett <ellis.trickett@gmail.com>
2020-08-27 21:02:42 +0100

    create product route

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator')
+
 
 const Product = require('../../models/product');
 
-//@route   post api/products/create
+//@route   POST api/products/create
 //@desc    create a product
 //@access  Private
 
-// router.post('/create', [auth, [
-//   check('name', 'Name is required').not().isEmpty(),
-//   check('photo', 'Photo is required').not().isEmpty(),
-//   check('description', 'Description is required').not().isEmpty(),
-//   check('category', 'Category is required').not().isEmpty(),
-//   check('location', 'Location is required').not().isEmpty(),
-//   check('shipping', 'Shipping is required').not().isEmpty(),
-//   check('price', 'Price is required').not().isEmpty(),
-//   check('domesticShipping', 'Domestic Shipping is required').not().isEmpty(),
-//   check('internationalShipping', 'International Shipping is required').not().isEmpty(),
-// ] ], (req, res) => {
-//   const errors = validationResult(req);
-//   if(!errors.isEmpty()) {
-//     return res.status(400).json({ errors: errors.array() })
-//   }
-// });
+router.post('/create', [auth, [
+  check('name', 'Name is required').not().isEmpty(),
+  check('photo', 'Photo is required').not().isEmpty(),
+  check('description', 'Description is required').not().isEmpty(),
+  check('category', 'Category is required').not().isEmpty(),
+  check('location', 'Location is required').not().isEmpty(),
+  check('shipping', 'Shipping is required').not().isEmpty(),
+  check('price', 'Price is required').not().isEmpty(),
+  check('domesticShipping', 'Domestic Shipping is required').not().isEmpty(),
+  check('internationalShipping', 'International Shipping is required').not().isEmpty(),
+] ], (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+});
 
 
 //@route   GET api/products
