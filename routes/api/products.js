32439f87971ef1b5ed06884f04380c667531a533
Ellis Trickett <ellis.trickett@gmail.com>
2020-08-28 13:45:14 +0100

    deleting a product

--- routes/api/products.js
+++ routes/api/products.js
@@ -64,34 +64,63 @@ router.post('/', [auth, [
 router.get('/', async (req, res) => {
   try {
     const products = await Product.find().sort({ date: -1 })
-    res.json(products)
+    res.json(product)
   } catch(err) {
     console.error(err.message);
     res.status(500).send('Server Error')
   }
 });
 
-//@route   GET api/posts/:id
-//@desc    Get post by ID
+//@route   GET api/products/:id
+//@desc    Get product by ID
 //@access  Public
 
 router.get('/:id', async (req, res) => {
   try {
-    const products = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id);
 
-    if(!products) {
+    if(!product) {
       return res.status(404).json({ msg: 'Post not found' })
     }
 
-    res.json(products)
+    res.json(product)
   } catch(err) {
     console.error(err.message);
     if(err.kind === 'ObjectId') {
-      return res.status(404).json({ msg: 'Post not found' })
+      return res.status(404).json({ msg: 'product not found' })
     }
     res.status(500).send('Server Error')
   }
 });
 
+//@route   DELETE api/products/:id
+//@desc    Delete a product
+//@access  Public
+
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if(!product) {
+      return res.status(404).json({ msg: 'Product not found' })
+    }
+
+    // Check user
+    if(product.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not Authorized'})
+    }
+
+    await product.remove();
+  
+
+    res.json({ msg: 'Product removed'});
+  } catch(err) {
+    console.error(err.message);
+    if(err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Product not found' })
+    }
+    res.status(500).send('Server Error')
+  }
+});
 
 module.exports = router;
