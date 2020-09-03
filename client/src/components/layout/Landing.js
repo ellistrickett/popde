37ef5044076d28937dd95b3ea2f559a6d2fc721b
Ellis Trickett <ellis.trickett@gmail.com>
2020-09-03 15:40:50 +0100

    added landing component with background image

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import landingPage from'../../img/landing_page.jpg';
+
+const Landing = () => {
+  return (
+    <div className="bck-image">
+    </div>
+  )
+}
+
+export default Landing;
