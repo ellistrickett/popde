ebc64308c8495d3258c7cc8d91412d2b3c87e822
Ellis Trickett <ellis.trickett@gmail.com>
2020-09-24 16:38:13 +0100

    Follow button added and wired up to backend

--- client/src/components/product/product.js
+++ client/src/components/product/product.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import { getProduct } from '../../actions/product';
 import { addLike, removeLike } from '../../actions/like';
+import { followUser } from '../../actions/follow';
 
 
 const Product = ({ 
+  followUser,
   addLike, 
   removeLike,
   getProduct, 
@@ -22,6 +24,10 @@ const Product = ({
       </div>
       <button onClick={e => addLike(product._id)}>Like</button>
       <button onClick={e => removeLike(product._id)}>Unlike</button>
+      <div>
+        {product && product.username}
+      </div>
+      <button onClick={e => followUser(product.userId)}>Follow</button>
     </Fragment>
 
   )
@@ -36,4 +42,4 @@ const mapStateToProps = state => ({
   product: state.product
 })
 
-export default connect(mapStateToProps, { getProduct, addLike, removeLike } )(Product)
+export default connect(mapStateToProps, { getProduct, addLike, removeLike, followUser } )(Product)
